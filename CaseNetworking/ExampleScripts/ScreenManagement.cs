61cd1e0da7c6fb405ef1661547cdaf3554f3a096
Wael El Echi <echi.wael@gmail.com>
2019-04-24 11:54:15 +0100

    first

---
+++ CaseNetworking/ExampleScripts/ScreenManagement.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenManagement : MonoBehaviour
+{
+    public static ScreenManagement instance;
+
+    [SerializeField]
+    private Image screenImage;
+
+    [Header("Screen Image Sprites")]
+    [SerializeField]
+    private Sprite screenOnSprite;
+
+    [SerializeField]
+    private Sprite screenOffSprite;
+
+    private int connectionId = -1;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        Server.instance.clientDisconnectionEvent += OnScreenDisconnected;
+    }
+
+    public void OnScreenConnected(int id)
+    {
+        SetConnectionId(id);
+        screenImage.sprite = screenOnSprite;
+        InfoTextView.instance.SetInfo("Established connection with the screen APP");
+    }
+
+    public void OnScreenDisconnected(int id)
+    {
+        if (id == connectionId)
+        {
+            SetConnectionId(-1);
+            screenImage.sprite = screenOffSprite;
+            InfoTextView.instance.SetInfo("Connection lost with the screen APP");
+        }
+    }
+
+    public void SetConnectionId(int id)
+    {
+        connectionId = id;
+    }
+
+    public int GetScreenConnectionId()
+    {
+        return connectionId;
+    }
+
+    public bool screenConnected()
+    {
+        return connectionId != -1;
+    }
+}
---
+++ CaseNetworking/ExampleScripts/ScriptableObjects/Item.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/Item")]
+public class Item : ScriptableObject
+{
+    public int id;
+    public string itName;
+    public Sprite image;
+}
